6fb87d61d7d5f86bdb1bc75a306764fc64ee37e5
livanshu-minfy <livanshu.saini@minfytech.com>
2025-06-24 09:53:48 +0530

    copy pasting files

---
+++ github actions/take home/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const version = process.env.APP_VERSION || "1.0.1";
+  res.send(`Hello from our container! Version: ${version}. Deployed automatically! -- livanshu`);
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
